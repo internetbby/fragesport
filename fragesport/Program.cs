bb15de4351f8865cb8dcabf2c8a13c17468fd2b0
Linnea <linnea.alse@elev.ga.ntig.se>
2020-09-28 09:30:15 +0200

    fixade loop buggen, spelet är klart. Skulle kunna lägga in mer frågor men det är superficial förändringar

--- fragesport/Program.cs
+++ fragesport/Program.cs
@@ -32,32 +32,30 @@ namespace fragesport
 
             completeQuestions.Add("what is sake made from?");
             completeAnswers.Add(new string[]{"1)rice  ", "2)wheat  ", "3)flower  "});
-            completeCorrectAnswer.Add(1); // 
+            completeCorrectAnswer.Add(1); 
 
-
-
-            
-
+            completeQuestions.Add("What is the capital of Westeros in Game of Thrones?");
+            completeAnswers.Add(new string[]{"1)Qarth  ", "2)Meereen  ", "3)King's Landing  "});
+            completeCorrectAnswer.Add(3); 
 
 
 
             List<string> questions = new List<string>(completeQuestions);
             List<string[]> answers = new List<string[]>(completeAnswers);
             List<int> correctAnswer = new List<int>(completeCorrectAnswer); //duplicates a list so i can change them up and then return them to their full state after the quiz is done
-            int questionsListed =questions.Count;
-            int randomizer = Generator.Next(questionsListed);
             
 
-            string exit ="" ; 
+            string exit = "Y" ; 
 
-            while (exit != "n"){
+            while (exit != "N"){
+
+                Console.Clear();
 
                 Console.WriteLine("Welcome to the quiz!");
                 Console.ReadLine(); 
 
                 while (questions.Count>0){
-                    questionsListed = questions.Count ;  //gives the randomizer number to go by when randomizing
-                    randomizer = Generator.Next(questionsListed);
+                    int randomizer = Generator.Next(questions.Count);
                     System.Console.WriteLine(questions[randomizer]);
                     Console.WriteLine(String.Join("\n", answers[randomizer]));
                     int corr= correctAnswer[randomizer];
@@ -71,73 +69,40 @@ namespace fragesport
 
                 }
 
+                Console.WriteLine("score is " + score);
+
+                Console.ReadLine();
+
+                questions.AddRange(completeQuestions); //adda tillbaka alla i listan
+                answers.AddRange(completeAnswers);
+                correctAnswer.AddRange(completeCorrectAnswer);
+
                 System.Console.WriteLine("quiz done! wanna retry?");
                 System.Console.WriteLine("[Y/N]");
                 string exitAnswer = Console.ReadLine();
-                exit = exitAnswer.ToLower();
+                exit = exitAnswer.ToUpper();    
 
-
-                while (exit != "y" || exit != "n"){
+                while (exit != "Y" && exit != "N"){
                     System.Console.WriteLine("please input valid answer");
                     exitAnswer = Console.ReadLine();
-                    exit = exitAnswer.ToLower();
+                    exit = exitAnswer.ToUpper();
                 }
-
-
-                if (exit == "y"){
-
-                       questions = completeQuestions; 
-                       answers = completeAnswers; 
-                       correctAnswer = completeCorrectAnswer;
-                }  
+                if (exit == "N"){
+                    System.Console.WriteLine("thanks for playing the quiz!");
+                }
+                else if(exit == "Y"){
+                    System.Console.WriteLine("ok, time to replay :)!");
+                }
+                
 
 
             }
-           
-
-            
-
-            
-
-
-            
-
-            /*
-
-            QuizText("What is japanese Sake made from");
-            QuizAnswer("rice", "wheat", "flowers");
-
-            score+=Ask(1);
-
-            Console.ReadLine();
-            Console.Clear();
-
-            QuizText("What is the capital of Westeros in Game of Thrones?");
-            QuizAnswer("Qarth", "Meereen", "King's Landing");
-
-            score+=Ask(3);
-
-            
-            Console.ReadLine();
-            Console.Clear();*/
-
-
-            Console.WriteLine("score is " + score);
 
             Console.ReadLine();
         }
 
 
-        
-        static void QuizText(string question){
-            System.Console.WriteLine(question);
-
-        }
-
-        static void QuizAnswer(string answer1, string answer2, string answer3){
-           System.Console.WriteLine("1)"+ answer1+"   2)"+answer2+ "   3)"+answer3);
-        }
-
+    
         static int Ask(int correctAnswer) // checkar svaret+ ger poäng//
         {
             int input = playerTryParse();
